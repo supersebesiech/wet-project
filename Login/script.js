b3c4d82554d182ea62e358eada36977ebea72060
Matthew170304 <milav24@student.sdu.dk>
2025-10-01 17:59:15 +0200

    error messages Register page

--- Login/script.js
+++ Login/script.js
@@ -12,6 +12,9 @@ const submitLogin = document.getElementById('submitLogin');
 const submitRegister = document.getElementById('submitRegister');
 const submitPasswordReset = document.getElementById('submitPasswordReset');
 const date = document.getElementById('date');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const accept = document.getElementById('accept');
 
 showPasswordCheckbox.addEventListener('change', function() { //event listener for checkbox
   if (this.checked) {
@@ -23,6 +26,9 @@ showPasswordCheckbox.addEventListener('change', function() { //event listener fo
 
 submitRegister.addEventListener('click', function(e) {
   e.preventDefault();
+    const password1 = password.value;
+    const password2 = confirmPassword.value;
+    const terms = accept.checked;
     const dateOfBirth = new Date(date.value);
     const now = new Date();
     const eighteen = new Date(
@@ -30,11 +36,43 @@ submitRegister.addEventListener('click', function(e) {
         now.getMonth(),
         now.getDate()
     );
-    if (dateOfBirth > eighteen) {
-        errorAge.style.display = "block";  
-    } else {
-        errorAge.style.display = "none";   
+    if (dateOfBirth > eighteen) 
+    {
+      errorAge.style.display = "block";  
+    } 
+    else 
+    {
+      errorAge.style.display = "none";   
     }
 
-    
+    if (password1 === password2)
+    {
+      errorPasswordCheck.style.display = "none";
+    }
+    else 
+    {
+      errorPasswordCheck.style.display = "block";
+    }
+
+    if (/[A-Z]/.test(password1) && /[A-Z]/.test(password2))
+    {
+      errorPassword.style.display = "none";
+    }
+    else 
+    {
+      errorPassword.style.display = "block";
+    }
+
+    if (terms)
+    {
+      errorTerms.style.display = "none";
+    }
+    else
+    {
+      errorTerms.style.display = "block";
+    }
+});
+
+submitLogin.addEventListener('click', function(e) {
+  e.preventDefault();
 });
