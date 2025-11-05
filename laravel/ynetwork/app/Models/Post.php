e24c9bf5a60b18f7b34049358d7b9fc922ad3020
supersebesiech <ramon.schnarwiler@gmail.com>
2025-11-05 09:45:23 +0100

    working demo of post database. enter test page with /test

--- laravel/ynetwork/app/Models/Post.php
+++ laravel/ynetwork/app/Models/Post.php
@@ -10,7 +10,9 @@ class Post extends Model
     use HasFactory;
     //
     protected $table = 'posts';
-    protected $fillable = ['user_id',
+    protected $fillable = [
+        'user_id',
+        'title',
         'content'];
 
     public function user()
--- laravel/ynetwork/routes/web.php
+++ laravel/ynetwork/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -21,3 +22,18 @@ Route::get('/foryou', function () {
 Route::get('/profile', function () {
     return view('pages.profile');
 })->name('profile');
+
+
+Route::get('/test', [PostController::class, 'index'])->name('posts.index');
+// returns the form for adding a post
+Route::get('/test/posts/create', [PostController::class, 'create'])->name('posts.create');
+// adds a post to the database
+Route::post('/test/posts', [PostController::class, 'store'])->name('posts.store');
+// returns a page that shows a full post
+Route::get('/test/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// returns the form for editing a post
+Route::get('/test/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// updates a post
+Route::put('/test/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// deletes a post
+Route::delete('/test/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
