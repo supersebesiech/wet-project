6070db5c5c73534424d7a70639af4b553ba58884
Matthew170304 <milav24@student.sdu.dk>
2025-11-07 23:23:57 +0100

    register fix

--- 2.assigment/Ynetwork/app/Http/Controllers/RegisteredUserController.php
+++ 2.assigment/Ynetwork/app/Http/Controllers/RegisteredUserController.php
@@ -21,11 +21,10 @@ class RegisteredUserController extends Controller
             'first_name' => 'required',
             'last_name' => 'required',
             'email' => ['required', 'email', 'unique:users', 'confirmed'],
-            'password' => ['required', 'min:8', 'confirmed', Password::min(8),]
+            'password' => ['required', 'confirmed', Password::min(8)],
         ]))
         {
             throw ValidationException::withMessages([
-                'password' => 'Provided email or password are incorrect.'
             ]);
         };
 
--- 2.assigment/Ynetwork/resources/views/auth/register.blade.php
+++ 2.assigment/Ynetwork/resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <x-login-layout type="Create new profile">
     {{-- Form --}}
-    <form method="POST" action="/login" class="pt-14  ">
+    <form method="POST" action="/register" class="pt-14  ">
         @csrf
         <div class="flex flex-col items-center gap-5">
             <div class="flex flex-col gap-5">
@@ -12,7 +12,7 @@
                 <x-form-input  id="password" name="password" required type="password" placeholder="Password"></x-form-input>
                 <x-form-input id="password_confirmation" name="password_confirmation" required type="password" placeholder="Confirm password"></x-form-input>
                 <x-form-error name="password"></x-form-error>
-                <x-form-input type="date"></x-form-input>
+                <x-form-input id="date" name="date" type="date"></x-form-input>
             </div>
             <div class="flex flex-col gap-0.5">
                 <button class="bg-black text-white rounded-full py-2 px-4" type="submit">Register</button>
