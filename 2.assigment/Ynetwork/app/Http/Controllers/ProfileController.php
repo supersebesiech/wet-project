3404b2a009568117b717c66168e0a1aaca96afa6
Matthew170304 <milav24@student.sdu.dk>
2025-11-16 16:55:49 +0100

    profile edit and fixed mail

--- 2.assigment/Ynetwork/app/Http/Controllers/ProfileController.php
+++ 2.assigment/Ynetwork/app/Http/Controllers/ProfileController.php
@@ -11,20 +11,35 @@ class ProfileController extends Controller
 {
     /**
      * Display posts for "Profile" page.
-     * 
+     *
      * @return \Illuminate\Http\Response
      */
     public function profile()
     {
         $profiledata = auth()->user();
-        // SORT POSTS DESCENDING BY CREATED_AT
-        //$posts = Post::all()->sortByDesc('created_at');
 
         $posts = Post::with('user')
-                 ->where('user_id', $profiledata->id)
-                 ->latest()
-                 ->get();
+            ->where('user_id', $profiledata->id)
+            ->latest()
+            ->get();
 
-        return view('user.profile', compact('posts'), compact('profiledata'));
+        return view('user.profile', compact('posts', 'profiledata'));
+    }
+
+    public function editUser(Request $request)
+    {
+        $user = Auth::user();
+
+        $validated = $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'bio' => ['nullable', 'string'],
+        ]);
+
+        $user->email = $validated['email'];
+        $user->bio   = $validated['bio'];
+
+        $user->save();
+
+        return redirect('/profile');
     }
 }
--- 2.assigment/Ynetwork/routes/web.php
+++ 2.assigment/Ynetwork/routes/web.php
@@ -29,6 +29,7 @@ Route::get('/for-you', [PostController::class, 'foryou']
 
 Route::get('/profile',[ProfileController::class, 'profile']
 )->name('user.profile');
+Route::post('/edit',[ProfileController::class, 'editUser']);
 
 // searched users profile page
 Route::get('/users/{id}', [RegisteredUserController::class, 'show'])->name('users.show');
