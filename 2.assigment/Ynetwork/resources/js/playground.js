670e77d20ffd8f161d6664befc67fd8f2323971e
supersebesiech <ramon.schnarwiler@gmail.com>
2025-12-11 09:33:42 +0100

    demo animations on front

--- 2.assigment/Ynetwork/resources/js/playground.js
+++ 2.assigment/Ynetwork/resources/js/playground.js
@@ -1,8 +1,36 @@
-import { animate, stagger, splitText } from 'animejs';
+import { animate, splitText, stagger, utils, svg, spring, createTimeline } from "animejs";
 
-const { chars } = splitText('.ramon', { words: false, chars: true });
-const { charts } = splitText('.roman', { words: false, chars: true });
-const boio = (chars) => animate(chars,{
+const { chars } = splitText('.title-target', {
+  chars: { wrap: 'visible' },
+  debug: false,
+ });
+
+const $formular = utils.$('.formular-container');
+const [ $path1, $path2 ] = utils.$('polygon');
+
+
+
+
+
+
+
+
+
+
+
+  const shift = () => animate(chars, {
+    y: [
+      { to: ['100%', '0%'] },
+      { to: '-100%', delay: 1000, ease: 'in(3)' },
+      { to: ['100%', '0%'] },
+    ],
+    duration: 750,
+    ease: 'out(3)',
+    delay: stagger(50),
+    
+  });
+
+ const turn = () => animate(chars, {
   // Property keyframes
   y: [
     { to: '-2.75rem', ease: 'outExpo', duration: 600 },
@@ -16,7 +44,115 @@ const boio = (chars) => animate(chars,{
   delay: stagger(50),
   ease: 'inOutCirc',
   loopDelay: 1000,
-  loop: true
+  loop: false
 });
-boio(chars);
-boio(charts);
+
+ 
+
+const zoom = () => animate(chars[0], {
+  x: [{to: ((window.innerWidth/2)-chars[0].getBoundingClientRect().x) - (chars[0].getBoundingClientRect().width/2), ease: 'inexpo'}], 
+  y: [{to: ((window.innerHeight/2)-chars[0].getBoundingClientRect().y) - (chars[0].getBoundingClientRect().height/2), ease: 'inexpo'}],
+  scale: [
+    { to: 4, ease: 'inexpo' },
+  ],
+  loop: false,
+  duration: 1000,
+  onComplete: () => { drawsvgY().play(); },
+  autoPlay: false,
+});
+
+const drawsvgY = () => animate($path1, {
+  opacity: [{ to: 1 }],
+  duration: 100,
+  easing: 'linear',
+  onComplete: () => { deleteStuff().play(); },
+})
+
+
+const deleteStuff = () => animate(chars[0], {
+  opacity: 0,
+  duration: 4,
+  easing: 'linear',
+  onComplete: () => { startWaitingAnimation(); },
+})
+
+const deleteBackground = () => animate($formular, {
+  opacity: 0,
+  duration: 1000,
+  easing: 'linear',
+  autoplay: false,
+})
+
+  const swarm = () => animate(chars, {
+    y: [
+      { from: utils.random(-500, 500) },
+    ],
+    x: [
+      { from: utils.random(-500, 500) },
+    ],
+    duration: () => utils.random(2000, 2800),
+    delay: () => utils.random(0, 400),
+    ease: 'outElastic(1, .5)',
+    autoplay : false,
+  })
+
+ function loopAnimation() {
+   sleep(2000).then(() => {zoom().play()});
+ }
+
+
+
+ function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+ 
+// polygon animation
+
+ 
+function animateRandomPoints() {
+  // Update the points attribute on #path-2
+  utils.set($path2, { points: generatePoints() });
+  // Morph the points of #path-1 into #path-2
+  animate($path1, {
+    points: svg.morphTo($path2),
+    ease: 'inOutCirc',
+    duration: 1200,
+    onComplete: animateRandomPoints
+  });
+}
+
+// Start the animation
+
+
+function startWaitingAnimation() {
+//$path1.style.opacity = '1';
+animateRandomPoints();
+}
+
+// A function to generate random points on #path-2 on each iteration
+// For demo purpose only
+function generatePoints() {
+  const total = utils.random(4, 20);
+  const r1 = utils.random(20, 60);
+  const r2 = 100;
+  const isOdd = n => n % 2;
+  let points = '';
+  for (let i = 0, l = isOdd(total) ? total + 1 : total; i < l; i++) {
+    const r = isOdd(i) ? r1 : r2;
+    const a = (2 * Math.PI * i / l) - Math.PI / 2;
+    const x = 100 + utils.round(r * Math.cos(a), 0);
+    const y = 100 + utils.round(r * Math.sin(a), 0);
+    points += `${x},${y} `;
+  }
+  return points;
+}
+
+function customComosition (){
+
+  sleep(3000).then(() => {zoom().play(), deleteBackground().play()});
+} 
+deleteBackground().play();
+//customComosition();
+//swarm().play();
+//loopAnimation();
+//startWaitingAnimation();
--- 2.assigment/Ynetwork/resources/views/components/login-layout.blade.php
+++ 2.assigment/Ynetwork/resources/views/components/login-layout.blade.php
@@ -23,16 +23,41 @@
 
 </head>
 
-<body class="bg-cover-section w3-display-container w3-center"> {{-- Title --}} <div
-        class="w3-container w3-display-middle w3-animate-opacity">
-        <div class="w3-col w3-center">
-            <h1 class="ramon w3-row w3-xxxlarge w3-bold s12 m12 l12 ">Ynetwork</h1>
-            <h2 class="roman w3-row w3-xxlarge w3-bold s12 m12 l12 funky-animation-text">{{ $type }}</h2> {{$slot}}
+<body class="bg-cover-section w3-display-container w3-center">
+
+    <div class="w3-overlay w3-display-container w3-show" style="pointer-events:none; background-color: transparent !important;">
+        <div class="w3-display-middle">
+            <svg width="200" height="200" viewBox="0 0 200 200">
+                <g stroke-width="2" stroke="white" stroke-linejoin="round" fill="black" fill-rule="evenodd">
+                    <polygon style="opacity: 0" id="path-1"
+                        points="42,47 76,47 99,100 101,108 103,100 126,47 158,47 114,134 114,181 84,181 84,135">
+                    </polygon>
+                    <polygon style="opacity: 0" id="path-2"
+                        points="100,56 118,90 152,108 118,126 100,160 82,126 48,108 82,90">
+                    </polygon>
+
+                </g>
+            </svg>
         </div>
     </div>
+
+   
+    <div class="w3-container w3-display-middle w3-animate-opacity formular-container" style="opacity: 1" >
+        <div class="w3-col w3-center animation-container">
+            <h1 class="title-target w3-xxxlarge w3-row w3-bold"
+                style="-webkit-text-stroke: 0.5px white;">Ynetwork</h1>
+
+            <h2 class="w3-row w3-xlarge w3-bold funky-animation-text">{{ $type }}</h2>
+
+            {{ $slot }}
+        </div>
+    </div>
+
     <footer class="w3-display-bottomleft w3-text-white w3-small">
         <p>&copy;2025 Ynetwork | Contact: y@network.com</p>
     </footer>
+
 </body>
+
 
 </html>
